ecf995fb6bd78bedfe9006980fa44c6589fb4e1a
felixojiambo <felixojiamboe@gmail.com>
2024-12-11 20:42:39 +0300

    wrap your application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,24 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../store'
 
-// We will add the Header, Footer, DarkModeToggle in a later step
-// For now, let's just verify the store integration
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+export const metadata = {
+  title: 'My App',
+  description: 'E-commerce application',
 }
 
-export default MyApp
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Provider store={store}>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  )
+}
